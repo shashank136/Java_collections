69ac6eef8b0edabd74f6ac67d4220213c9c7a3a1
Shashank Kumar <shashank136.sk@gmail.com>
2019-01-20 00:34:14 +0530

    error correction

--- collections/ComparableDemo.java
+++ collections/ComparableDemo.java
@@ -24,13 +24,13 @@ class Student implements Comparable<Student>{
 
 public class ComparableDemo{
 	public static void main(String args[]){
-		ArrayList<String> al = new ArrayList<>();
+		ArrayList<Student> al = new ArrayList<>();
 
 		al.add(new Student(101,"Vijay",23));  
 		al.add(new Student(106,"Ajay",27));  
 		al.add(new Student(105,"Jai",21));
 
-		Collection.sort(al);
+		Collections.sort(al);
 		for(Student st:al){
 			System.out.println(st.rollno+" "+st.name+" "+st.age);
 		}
